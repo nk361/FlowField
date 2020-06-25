b6c5fd9cd07b6114b536038586d39d2bf8ff7c71
Neil Kline <lolnk361@gmail.com>
2020-06-25 02:58:51 -0400

    Truly fixed the issues with float inaccuracies and converted the vector list to a two dimensional list.

--- 2DFlowField.js
+++ 2DFlowField.js
@@ -23,10 +23,10 @@ function threeDLerp(startVector, endVector, zeroToOneBetween) {
 }
 
 function pickedVectorLerp(vectors, zeroToOneBetween) {//get the current vector out of a linear gradient between an unknown amount of vectors{
-    if(zeroToOneBetween === 1)//when the value is 1, the last return line throws an index out of bounds as it should
-        return vectors[vectors.length - 1];
 
     let currentColorIndex = Math.floor(zeroToOneBetween / (1 / (vectors.length - 1)));
+    if(currentColorIndex === vectors.length - 1)//float inaccuracies often caused an index out of bounds in the last return
+        return vectors[vectors.length - 1];
     let currentZeroToOneBetween = (zeroToOneBetween - ((1 / vectors.length) * currentColorIndex)) * vectors.length;
     return threeDLerp(vectors[currentColorIndex], vectors[currentColorIndex + 1], currentZeroToOneBetween);
 }
@@ -37,7 +37,7 @@ let orange = new Vector3D(243, 156, 18);
 let red = new Vector3D(231, 76, 60);
 let colors = [green, yellow, orange, red];
 
-let arrowLength = 100 / 5;//20;
+let arrowLength = 100 / 5;
 function drawArrow(x, y, length, angle, gradientPosition) {
     //ctx.fillStyle = "rgb(" + 100 * Math.random() + ", " + 100 * Math.random() + ", " + 100 * Math.random() + ")";
     //ctx.fillRect(x, y, arrowLength, arrowLength);
@@ -58,38 +58,56 @@ function drawArrow(x, y, length, angle, gradientPosition) {
     ctx.fill();
 }
 
+function clamp(value, max, min = 0) {//Purely to deal with float inaccuracies that literally gave values less than min possible or greater than max possible
+    if(value > max)
+        return max;
+    if(value < min)
+        return min;
+    return value;
+}
+
 function scaleBetweenZeroAndOne(value, max, min = 0) {
-    if(value >= max - 0.00001)//float issues. If it was the same number as max, it gave 0.9999999999, instead of 1
-        return 1;
-    return 1 / (max - min) * (value - min);
+    return 1 / (Math.round(max) - Math.round(min)) * (Math.round(clamp(value, max, min)) - Math.round(min));
 }
 
 let spaceBetween = arrowLength / 4;
-let rows = Math.floor(ctx.canvas.height / (arrowLength) + spaceBetween) + 1;//19;
-let cols = Math.floor(ctx.canvas.width / (arrowLength) + spaceBetween) + 1;//39;
-let amountOfArrows = rows * cols;
+let rows = Math.floor(ctx.canvas.height / (arrowLength) + spaceBetween) + 1;
+let cols = Math.floor(ctx.canvas.width / (arrowLength) + spaceBetween) + 1;
 
 let vectors = [];
 
-for(let i = 0; i < amountOfArrows; i++)
-{
-    vectors.push(new Vector3D(Math.random() * 200 - 100, Math.random() * 200 - 100, Math.random() * 200 - 100));
+for(let col = 0; col < cols; col++) {
+    vectors.push([]);
+    for (let row = 0; row < rows; row++) {
+        //vectors[col].push(new Vector3D(Math.random() * 200 - 100, Math.random() * 200 - 100, Math.random() * 200 - 100));//currently only uses the x and y
+        //console.log("col: " + col + " row: " + row);
+        if(col === 0 && row === 0)
+            vectors[col].push(new Vector3D(1, 1, 0));//currently only uses the x and y
+        else if(col === 0)
+            vectors[col].push(new Vector3D(vectors[0][row - 1].x + 1, vectors[0][row - 1].y, vectors[0][row - 1].z));//currently only uses the x and y
+        else if(row === 0)
+            vectors[col].push(new Vector3D(vectors[col - 1][0].x, vectors[col - 1][0].y + 1, vectors[col - 1][0].z));//currently only uses the x and y
+        else
+            vectors[col].push(new Vector3D((vectors[col][row - 1].x + vectors[col - 1][row].x) / 2, (vectors[col][row - 1].y + vectors[col - 1][row].y) / 2, vectors[col - 1][row - 1].z));//currently only uses the x and y
+    }
 }
 
-let min = vectors[0].magnitude();//using default center of 0, 0, 0
-let max = vectors[0].magnitude();//using default center of 0, 0, 0
-for(let i = 1; i < amountOfArrows; i++) {//skip first one{
-    let currentMagnitude = vectors[i].magnitude();//using default center of 0, 0, 0
-    if(currentMagnitude < min)
-        min = currentMagnitude;
-    else if(currentMagnitude > max)
-        max = currentMagnitude;
+let min = vectors[0][0].magnitude();//using default center of 0, 0, 0
+let max = vectors[0][0].magnitude();//using default center of 0, 0, 0
+for(let col = 0; col < cols; col++) {
+    for(let row = 1; row < rows; row++) {//skip first one{
+        let currentMagnitude = vectors[col][row].magnitude();//using default center of 0, 0, 0
+        if (currentMagnitude < min)
+            min = currentMagnitude;
+        else if (currentMagnitude > max)
+            max = currentMagnitude;
+    }
 }
 
 for(let col = 0; col < cols; col++) {
     for(let row = 0; row < rows; row++) {
         let currentX = (arrowLength + spaceBetween) * col;
         let currentY = (arrowLength + spaceBetween) * row;
-        drawArrow(currentX, currentY, arrowLength, vectors[col * rows + row].getXPlaneAngle(), scaleBetweenZeroAndOne(vectors[col * rows + row].magnitude(), max, min));
+        drawArrow(currentX, currentY, arrowLength, vectors[col][row].getXPlaneAngle(), scaleBetweenZeroAndOne(vectors[col][row].magnitude(), max, min));
     }
 }
